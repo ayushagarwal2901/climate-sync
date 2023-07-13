4869087d50d68fb933defee8f91924daec50f801
Ayush Agarwal <ayushagarwal2901@gmail.com>
2023-07-13 21:21:22 +0530

    Created .env file

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = '64b79e8b361ec8dfb642df96189df503';
 
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
         params: {
             q: query,
             units: 'metric',
-            APPID: API_KEY,
+            APPID: process.env.REACT_APP_API_KEY,
         }
     });
     return data;
